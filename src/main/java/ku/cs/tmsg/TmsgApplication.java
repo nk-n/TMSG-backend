4350c05f6914b93949e4b6a882639188b9f723b3
nk-n <narakorn458@gmail.com>
2025-10-04 19:35:05 +0700

    chore: initialize project structure

---
+++ src/main/java/ku/cs/tmsg/TmsgApplication.java
@@ -0,0 +1,13 @@
+package ku.cs.tmsg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmsgApplication.class, args);
+	}
+
+}
