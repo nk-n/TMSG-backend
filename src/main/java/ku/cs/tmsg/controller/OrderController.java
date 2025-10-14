07909538923012f88ec5611d39c31398b13d744e
Rugsit <rugsit.nest@gmail.com>
2025-10-14 18:08:54 +0700

    feat(api): add GET /api/order/{status} for get order by status

--- src/main/java/ku/cs/tmsg/controller/OrderController.java
+++ src/main/java/ku/cs/tmsg/controller/OrderController.java
@@ -1,6 +1,8 @@
 package ku.cs.tmsg.controller;
 
 import ku.cs.tmsg.dto.request.OrderCreate;
+import ku.cs.tmsg.dto.response.ApiResponse;
+import ku.cs.tmsg.dto.response.OrderResponse;
 import ku.cs.tmsg.service.CarService;
 import ku.cs.tmsg.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,10 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -35,6 +34,17 @@ public class OrderController {
         } catch (Exception e) {
             System.err.println(e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("create order failed: " + e.getMessage());
+        }
+    }
+
+    @GetMapping("/{status}")
+    public ResponseEntity<ApiResponse<List<OrderResponse>>> GetOrder(@PathVariable String status) {
+        try {
+            List<OrderResponse> orders = orderService.getOrder(status);
+            return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<List<OrderResponse>>("get orders success", orders));
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse<List<OrderResponse>>("get orders failed: " + e.getMessage(), null));
         }
     }
 }
---
+++ src/main/java/ku/cs/tmsg/dto/response/OrderDriverResponse.java
@@ -0,0 +1,9 @@
+package ku.cs.tmsg.dto.response;
+
+import lombok.Data;
+
+@Data
+public class OrderDriverResponse {
+    private String name;
+    private String tel;
+}
