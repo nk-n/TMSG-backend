6f51cd3c2b34608edf724b2c665375d08c725cb6
nrkn <narakorn458@gmail.com>
2025-10-15 13:44:02 +0700

    feat(liff-delivery): add API endpoint to update delivery status

--- src/main/java/ku/cs/tmsg/controller/DriverTaskController.java
+++ src/main/java/ku/cs/tmsg/controller/DriverTaskController.java
@@ -1,14 +1,16 @@
 package ku.cs.tmsg.controller;
 
+import ku.cs.tmsg.dto.request.TaskStatusUpdateRequest;
+import ku.cs.tmsg.dto.response.OrderStatusResponse;
 import ku.cs.tmsg.dto.response.TaskResponse;
+import ku.cs.tmsg.exception.DatabaseException;
 import ku.cs.tmsg.service.DriverTaskService;
+import ku.cs.tmsg.service.OrderStatusService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,11 +21,23 @@ public class DriverTaskController {
     @Autowired
     private DriverTaskService driverTaskService;
 
+    @Autowired
+    private OrderStatusService orderStatusService;
+
     @GetMapping("/new")
     public ResponseEntity<List<TaskResponse>> newTask(@RequestHeader("Authorization") String authHeader) {
         try {
             return ResponseEntity.ok().body(driverTaskService.getNewTasks(authHeader.substring(7)));
         } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(null);
+        }
+    }
+
+    @PostMapping("/status/update")
+    public ResponseEntity<OrderStatusResponse>  updateStatus(@RequestBody TaskStatusUpdateRequest request) {
+        try {
+            return ResponseEntity.ok(orderStatusService.setOrderStatus(request));
+        } catch (DatabaseException e) {
             return ResponseEntity.internalServerError().body(null);
         }
     }
